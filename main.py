a2b9495243bca63b32c6bb5132fc889172c6a1bf
Роман <suvorovroman228@gmail.com>
2023-04-23 14:11:20 +0300

    beautiful logic v1.0

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ import disnake
 import requests
 from disnake.ext import commands
 import openai
+import datetime
 
 temp = open('token', 'r').readlines()[1]
 openai.api_key = temp
-bot = commands.Bot(command_prefix='!', intents=disnake.Intents.all(),
-                   activity=disnake.Game('попе пальчиком', status=disnake.Status.online))
+bot = commands.Bot(
+    command_prefix='!', intents=disnake.Intents.all(),
+    activity=disnake.Game('PyCharm', status=disnake.Status.dnd)
+)
 
 bot.remove_command('help')
 
@@ -18,8 +21,37 @@ async def on_ready():
 
 @bot.slash_command(description='Помогает вам')
 async def help(inter):
-    msg = "Привет, я чем то помогаю вам"
-    await inter.response.send_message(msg)
+    embed = disnake.Embed(title="Список команд",
+                          color=disnake.Color.random())
+    embed.add_field(
+        name="/cat",
+        value="Присылает милого котика",
+        inline=False
+    )
+    embed.add_field(
+        name="/dog",
+        value="Присылает милого пёсика",
+        inline=False
+    )
+    embed.add_field(
+        name="/server",
+        value="Данные о сервере",
+        inline=False
+    )
+    embed.add_field(
+        name="/question",
+        value="Задай вопрос а бот на него ответит",
+        inline=False
+    )
+    embed.set_author(
+        name="Умнейший бот",
+        icon_url="https://avatars.mds.yandex.net/i?id=d0bb10ef328847ecd137d5813a022aad14eafa96-5714527-images-thumbs&ref=rim&n=33&w=150&h=150",
+    )
+    embed.set_footer(
+        text=f"{datetime.datetime.now().date()}",
+        icon_url="https://avatars.mds.yandex.net/i?id=d0bb10ef328847ecd137d5813a022aad14eafa96-5714527-images-thumbs&ref=rim&n=33&w=150&h=150",
+    )
+    await inter.send(embed=embed)
 
 
 @bot.slash_command(description='Показывает милого котика')
@@ -28,9 +60,15 @@ async def cat(inter):
     data = a.json()
     embed = disnake.Embed(title="Милый котик Nya",
                           color=disnake.Color.random())
-    embed.set_image(url=data[0]['url'])
+    embed.set_image(
+        url=data[0]['url']
+    )
     embed.set_author(
         name="Умнейший бот",
+        icon_url="https://avatars.mds.yandex.net/i?id=d0bb10ef328847ecd137d5813a022aad14eafa96-5714527-images-thumbs&ref=rim&n=33&w=150&h=150",
+    )
+    embed.set_footer(
+        text=f"{datetime.datetime.now().date()}",
         icon_url="https://avatars.mds.yandex.net/i?id=d0bb10ef328847ecd137d5813a022aad14eafa96-5714527-images-thumbs&ref=rim&n=33&w=150&h=150",
     )
     await inter.send(embed=embed)
@@ -47,20 +85,49 @@ async def dog(inter):
         name="Умнейший бот",
         icon_url="https://avatars.mds.yandex.net/i?id=d0bb10ef328847ecd137d5813a022aad14eafa96-5714527-images-thumbs&ref=rim&n=33&w=150&h=150",
     )
+    embed.set_footer(
+        text=f"{datetime.datetime.now().date()}",
+        icon_url="https://avatars.mds.yandex.net/i?id=d0bb10ef328847ecd137d5813a022aad14eafa96-5714527-images-thumbs&ref=rim&n=33&w=150&h=150",
+    )
     await inter.send(embed=embed)
 
 
 @bot.slash_command()
 async def server(inter):
-    await inter.response.send_message(
-        f"Название сервера {inter.guild.name}\nУчастников: {inter.guild.member_count}"
+    embed = disnake.Embed(
+        color=disnake.Color.random()
     )
+    if inter.guild.icon:
+        embed.set_author(
+            name=inter.guild.name,
+            icon_url=inter.guild.icon.url,
+        )
+        embed.set_image(
+            inter.guild.icon.url
+        )
+        embed.add_field(name=f'Колличество участников: {inter.guild.member_count}',
+                        value="",
+                        inline=False
+                        )
+        embed.set_footer(
+            text=f"{datetime.datetime.now().date()}",
+            icon_url="https://avatars.mds.yandex.net/i?id=d0bb10ef328847ecd137d5813a022aad14eafa96-5714527-images-thumbs&ref=rim&n=33&w=150&h=150",
+        )
 
-
-@bot.slash_command()
-async def test(inter):
-    embed = disnake.Embed(title="Title", description="description", url="https://yandex.ru",
-                          color=disnake.Color.dark_blue())
+        #     f"Название сервера {inter.guild.name}\nУчастников: {inter.guild.member_count}\n {inter.guild.icon.url}"
+    else:
+        embed.set_author(
+            name=inter.guild.name,
+            icon_url="https://avatars.mds.yandex.net/i?id=d0bb10ef328847ecd137d5813a022aad14eafa96-5714527-images-thumbs&ref=rim&n=33&w=150&h=150",
+        )
+        embed.add_field(name=f'Колличество участников: {inter.guild.member_count}',
+                        value="",
+                        inline=False
+                        )
+        embed.set_footer(
+            text=f"{datetime.datetime.now().date()}",
+            icon_url="https://avatars.mds.yandex.net/i?id=d0bb10ef328847ecd137d5813a022aad14eafa96-5714527-images-thumbs&ref=rim&n=33&w=150&h=150",
+        )
     await inter.send(embed=embed)
 
 
@@ -77,6 +144,10 @@ async def question(inter, вопрос: str):
                           color=disnake.Color.green())
     embed.set_author(
         name="Умнейший бот",
+        icon_url="https://avatars.mds.yandex.net/i?id=d0bb10ef328847ecd137d5813a022aad14eafa96-5714527-images-thumbs&ref=rim&n=33&w=150&h=150",
+    )
+    embed.set_footer(
+        text=f"{datetime.datetime.now().date()}",
         icon_url="https://avatars.mds.yandex.net/i?id=d0bb10ef328847ecd137d5813a022aad14eafa96-5714527-images-thumbs&ref=rim&n=33&w=150&h=150",
     )
     await inter.followup.send(embed=embed)
