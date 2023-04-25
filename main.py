7f7357939df5456b69d86bf0cc82dc19c5c7845f
IXITMANI <suvorovroman228@gmail.com>
2023-04-25 15:00:05 +0300

    secret token logic v1.0

--- main.py
+++ main.py
@@ -5,7 +5,12 @@ import requests
 from disnake.ext import commands
 import openai
 
-openai.api_key = open('token', 'r').readlines()[1][:-1]
+tk = ""
+temp_1 = open('token', 'r').readlines()[1][:-1]
+for item in temp_1:
+    tk += chr(ord(item) - 5)
+tk = tk[:-1]
+openai.api_key = tk
 bot = commands.Bot(command_prefix='!', intents=disnake.Intents.all())
 bot.load_extensions('cogs')
 
